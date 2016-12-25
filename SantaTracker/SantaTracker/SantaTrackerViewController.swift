d0d2f9b4bd0858d246448bf41bd488b0a0a56c0a
jairoeli <jairoelid@gmail.com>
2016-12-24 19:29:55 -0600

    Set up storyboard

---
+++ SantaTracker/SantaTracker/SantaTrackerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SantaTrackerViewController.swift
+//  SantaTracker
+//
+//  Created by Jairo Eli de Leon on 12/24/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SantaTrackerViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
